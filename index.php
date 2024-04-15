63f975bd10ff2747973218c0642520c7b489e5ca
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-15 14:29:34 +0200

    [Completed] Second step  -> Add style with Bootstrap and print my array in a table.

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $hotels = [
 ];
 
 # Let's see my array
-var_dump($hotels);
+#var_dump($hotels);
 
 #Loop in my array with foeach
 /* foreach ($hotels as $hotel) {
@@ -59,14 +59,53 @@ var_dump($hotels);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 
 <body>
     <div class="container">
-        <div><?php foreach ($hotels as $hotel) {
-            echo $hotel["name"] . " - " . $hotel["description"] . " | " . $hotel["parking"] . " - " . $hotel["vote"] . " - " . $hotel["distance_to_center"] . "<br>";
-        } ?></div>
+        <table class="table table-primary mt-auto border border-1">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Average Vote</th>
+                    <th scope="col">Center distance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!--Loop with foreach-->
+                <?php foreach ($hotels as $key => $hotel): ?>
+                    <tr>
+                        <td scope="row"><?php echo $hotel["name"] ?></td>
+                        <td><?php echo $hotel["description"] ?></td>
+
+                        <!--If my boolean is true i print Yes, if not No-->
+                        <td><?php if ($hotel["parking"]) {
+                            echo "Yes";
+                        } else {
+                            echo "No";
+                        }
+                        ; ?></td>
+
+                        <td><?php echo $hotel["vote"] ?></td>
+
+                        <!--I use number_format function to also show decimal places-->
+                        <td><?php echo number_format($hotel["distance_to_center"], 2) ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
 </body>
 
